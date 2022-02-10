f9a61027dad7618cd5a2fe65fa0ea9f308722e1a
Or Katz <katz3112@gmail.com>
2022-02-10 17:39:43 -0500

    reverting the folder strcture

--- js_packers_detector/examples/aes_ctr_example.js
+++
@@ -1,6 +0,0 @@
-
-var khawarezmifou =  ('1639-7537-4662-1478<--!-->391f00c002bba69bqs4fs8df865fe15466b8<--!-->9974-5263-1008-5189');  
-var khawarezmic =  'IgCi2LukWV/qVp4xYYSXrN7c/3saV7MynbTNZDtLk+enFMyCWalQqZEvxU0AVg60jMIy5CuXGi/lYnwNhnQc16XqcrtI3rTb4IV9AoCaMVNnUtiRGRYW68LHiLyLQ3PfCsAc8/i4hOjMN5UgAxbCcU3I7OPsvl5E4XR5YC3st5X5jEPOK11H0WUUfwYc29MEmvpaCe+hJA3uBGpjVJeg5xOMPPCLkPrT4hShs/DdsvwDg8wXMKw8yS0xQi8zoziQ1KIldhl4q3Rm5B0WimmWB6RKELvJcCjp4tiLkBxKpfA8/AEp5ss='; 
-var output = Aes.Ctr.decrypt(khawarezmic, khawarezmifou, 256); 
-document.write(output)
-
